013282f0e0bc48552890183736ba35b7f08a3198
Patryk Sztuczka <patryk.sztuczka@codeagency.pl>
2023-10-15 13:21:59 +0200

    1 Created basic flask api endpoint

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hello World!")
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+
+cors = CORS(app)
+
+app.config["CORS_HEADERS"] = "Content-Type"
+
+
+@app.route("/api/hello")
+@cross_origin()
+def hello_world():
+    return {"text": "Hello World!"}
+
+
+if __name__ == "__main__":
+    app.run(host="192.168.8.102", debug=True)
