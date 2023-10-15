137e775b3202e0dbce48c794973d16ec11b52335
Patryk Sztuczka <73931963+patryksztuczka@users.noreply.github.com>
2023-10-15 16:29:07 +0200

    5 Implemented basic image color converting (#6)
    Co-authored-by: Patryk Sztuczka <patryk.sztuczka@codeagency.pl>

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from flask import Flask, request, jsonify, Response
 from flask_cors import CORS, cross_origin
 import base64
+import cv2
 
 app = Flask(__name__)
 
 cors = CORS(app)
-
-# app.config["CORS_HEADERS"] = "Content-Type"
 
 
 @app.route("/api/hello")
@@ -19,11 +18,18 @@ def hello_world():
 @cross_origin()
 def process_image():
     content = request.json
-
     image_base64 = content["imageBase64"]
     image_bytes = base64.b64decode(image_base64)
+
     with open("imageToSave.png", "wb") as f:
         f.write(image_bytes)
+
+    img = cv2.imread("imageToSave.png")
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    cv2.imwrite("imageToSave.png", gray)
+
     return {"status": "success"}
 
 
